d15b97bdf191de08109283028632e20ae5c0a26f
Roman Wicky van Doyer <rwickyvd@gmail.com>
2022-11-29 17:38:16 -0500

    work

--- src/main/java/setup/Aggregation.java
+++ src/main/java/setup/Aggregation.java
@@ -1,10 +1,14 @@
 package setup;
 
+import com.mongodb.spark.rdd.MongoRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SparkSession;
 import org.bson.Document;
 import com.mongodb.spark.MongoSpark;
 import com.mongodb.spark.rdd.api.java.JavaMongoRDD;
+import scala.reflect.ClassManifestFactory$;
+
 import static java.util.Collections.singletonList;
 public final class Aggregation {
     public static void main(final String[] args) throws InterruptedException {
@@ -18,15 +22,20 @@ public final class Aggregation {
         JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
         // Load and analyze data from MongoDB
         JavaMongoRDD<Document> rdd = MongoSpark.load(jsc);
-        System.out.println(rdd.first().toJson());
+        //System.out.println(rdd.first().toJson());
         /*Start Example: Use aggregation to filter a RDD***************/
         JavaMongoRDD<Document> aggregatedRdd = rdd.withPipeline(
                 singletonList(
-                        Document.parse("{ $match: { \"trip_distance\" : \"1.7\" } }")));
+                        Document.parse("{ $match: { \"trip_distance\": { $gt: \"2.0\"} } }")));
+
+
+
+
+
         /*End Example**************************************************/
         // Analyze data from MongoDB
         System.out.println(aggregatedRdd.count());
-        System.out.println(aggregatedRdd.first().toJson());
+        System.out.println(aggregatedRdd.take(5));
         jsc.close();
     }
 }
